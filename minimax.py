bc45781e759b65e6cf7934666f76acd1f230f6a4
bersoy12 <56676780+bersoy12@users.noreply.github.com>
2021-06-01 12:45:07 +0300

    Create minimax.py

---
+++ minimax.py
@@ -0,0 +1,59 @@
+from copy import deepcopy
+import pygame
+
+WHITE = (255, 255, 255)
+RED = (255,0,0)
+
+def minimax(position, depth, runner, game):
+    if depth == 0 or position.winner() != None:
+        return position.evaluate(), position
+    
+    if runner.name == "R":
+        maxEval = float('-inf')
+        best_move = None
+        for move in get_all_moves(position, runner, game):
+            evaluation = minimax(move, depth-1, False, game)[0]
+            maxEval = max(maxEval, evaluation)
+            if maxEval == evaluation:
+                best_move = move
+        
+        return maxEval, best_move
+    else:
+        minEval = float('inf')
+        best_move = None
+        for move in get_all_moves(position, runner, game):
+            evaluation = minimax(move, depth-1, True, game)[0]
+            minEval = min(minEval, evaluation)
+            if minEval == evaluation:
+                best_move = move
+        
+        return minEval, best_move
+
+
+def simulate_move(piece, move, board, game):
+    board.move(piece, move[0], move[1])
+    return board
+
+
+def get_all_moves(board, agent, game):
+    moves = []
+
+    for piece in board.get_all_agents():
+        valid_moves = board.get_valid_moves(agent)
+        for direct, coord in valid_moves.items():
+            # draw_moves(game, board, piece)
+            temp_board = deepcopy(board)
+            temp_agent = temp_board.get_agent(piece.row, piece.col)
+            new_board = simulate_move(temp_agent, direct, temp_board, game)
+            moves.append(new_board)
+    
+    return moves
+
+
+def draw_moves(game, board, piece):
+    valid_moves = board.get_valid_moves(piece)
+    board.draw(game.win)
+    pygame.draw.circle(game.win, (0,255,0), (piece.x, piece.y), 50, 5)
+    game.draw_valid_moves(valid_moves.keys())
+    pygame.display.update()
+    #pygame.time.delay(100)
